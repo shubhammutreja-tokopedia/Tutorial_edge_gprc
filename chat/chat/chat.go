7459adb072a9e5cd51124e98aabf910152341410
shubhammutreja-tokopedia <80829908+shubhammutreja-tokopedia@users.noreply.github.com>
2021-03-24 18:34:26 +0530

    Update chat.go

--- chat/chat/chat.go
+++ chat/chat/chat.go
@@ -9,6 +9,6 @@ type Server struct {
 }
 
 func (s *Server) SayHello(ctx context.Context, msg *Message) (*Message, error) {
-	log.Printf("Recived mssg : %s", message.Body)
+	log.Printf("Recived mssg : %s", msg.Body)
 	return &Message{Body: "Hello from server go"}, nil
 }
