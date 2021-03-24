9ec5b68ba70111b82bbc001d821ab063980263aa
shubhammutreja-tokopedia <80829908+shubhammutreja-tokopedia@users.noreply.github.com>
2021-03-24 18:28:03 +0530

    Add files via upload

---
+++ chat/chat/chat.go
@@ -0,0 +1,14 @@
+package chat
+
+import (
+	"context"
+	"log"
+)
+
+type Server struct {
+}
+
+func (s *Server) SayHello(ctx context.Context, msg *Message) (*Message, error) {
+	log.Printf("Recived mssg : %s", message.Body)
+	return &Message{Body: "Hello from server go"}, nil
+}
